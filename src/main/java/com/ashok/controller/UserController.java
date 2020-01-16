088312071531e3acf46209e205a4433dc0c86b06
Java-Project-03-9AM <48667032+Java-Project-03-9AM@users.noreply.github.com>
2020-01-16 10:51:22 +0530

    Removed sop stmt

--- src/main/java/com/ashok/controller/UserController.java
+++ src/main/java/com/ashok/controller/UserController.java
@@ -13,7 +13,6 @@ public class UserController {
 
 	@RequestMapping(value = "/displayForm.htm", method = RequestMethod.GET)
 	public String displayForm(Model model) {
-		System.out.println("this is displayForm() method");
 		model.addAttribute("user", new User());
 		return "userForm";
 	}
