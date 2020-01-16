2e89aef44bb9f758b6b3fd2476856d02e7f3cdc5
Java-Project-03-9AM <48667032+Java-Project-03-9AM@users.noreply.github.com>
2020-01-16 11:01:13 +0530

    Added sop for hi

--- src/main/java/com/ashok/controller/UserController.java
+++ src/main/java/com/ashok/controller/UserController.java
@@ -13,6 +13,7 @@ public class UserController {
 
 	@RequestMapping(value = "/displayForm.htm", method = RequestMethod.GET)
 	public String displayForm(Model model) {
+		System.out.println("hi");
 		model.addAttribute("user", new User());
 		return "userForm";
 	}
