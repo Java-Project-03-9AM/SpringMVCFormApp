081b5abcfd8effe24105ac2d9030c5b80bdc8ef7
Raj <raj.b@tcs.com>
2020-01-16 11:23:40 +0530

    Removed sop and unused variable

--- src/main/java/com/ashok/controller/UserController.java
+++ src/main/java/com/ashok/controller/UserController.java
@@ -13,8 +13,6 @@ public class UserController {
 
 	@RequestMapping(value = "/displayForm.htm", method = RequestMethod.GET)
 	public String displayForm(Model model) {
-		System.out.println("hi");
-		int i = 10;
 		model.addAttribute("user", new User());
 		return "userForm";
 	}
